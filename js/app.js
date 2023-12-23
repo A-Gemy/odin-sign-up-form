f402218bcbc29ae3e84482c1c729fa80c9fe6dbb
Ahmed Gamal <ahmgy.mail@gmail.com>
2023-12-23 23:57:26 +0200

    Add function to show passwords

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("Hello, I'm here...!");
+// Variables
+const checkBox = document.getElementById("show");
+const password = document.getElementById("password");
+const ConfirmPassword = document.getElementById("c-password");
+
+/* Toggles the visibility of two password input fields based on the state of a checkbox type.*/
+checkBox.onclick = function () {
+  if (checkBox.checked) {
+    password.type = "text";
+    ConfirmPassword.type = "text";
+  } else {
+    password.type = "password";
+    ConfirmPassword.type = "password";
+  }
+};
